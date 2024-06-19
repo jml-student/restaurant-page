bc1dc3f1d00f55d03947167bb02efbeee38760f0
jml-student <student741@proton.me>
2024-06-19 18:27:58 +0200

    Add dist folder

--- src/loadHome.js
+++ src/loadHome.js
@@ -3,18 +3,24 @@ import restaurantImg from './img/restaurant-img.png'
 export function loadHome() {
     const content = document.querySelector('#content');
 
-    const homeButton = document.querySelector('#home');
+    const homeFirstBlock = document.createElement('div');
+    const homeSecondBlock = document.createElement('div');
 
-    const homeContainer = document.createElement('div');
+    const title = document.createElement('div');
+    title.classList = 'home-title';
+    title.textContent = 'Welcome to Burger Haven!';
 
     const image = document.createElement('img');
     image.setAttribute('src', restaurantImg);
 
     const text = document.createElement('p');
-    text.textContent = 'This restaurant is awesome';
+    text.textContent = `At Burger Haven, we believe that a great burger is more than just a meal—it's an experience. Nestled in the heart of New York city, our restaurant combines the freshest ingredients, innovative recipes, and a passion for culinary excellence to bring you burgers like no other.`;
 
-    homeContainer.appendChild(image);
-    homeContainer.appendChild(text);
+    homeFirstBlock.appendChild(title);
+    homeFirstBlock.appendChild(image);
 
-    content.appendChild(homeContainer);
+    homeSecondBlock.appendChild(text);
+
+    content.appendChild(homeFirstBlock);
+    content.appendChild(homeSecondBlock);
 }
